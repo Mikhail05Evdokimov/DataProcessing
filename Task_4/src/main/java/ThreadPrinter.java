14403d9795f00b648e32537fa54878db868180e4
Mikhail <evdokimm05@gmail.com>
2023-09-20 11:28:11 +0700

    initial commit for task_4

---
+++ Task_4/src/main/java/ThreadPrinter.java
@@ -0,0 +1,18 @@
+public class ThreadPrinter extends Thread{
+
+    private boolean interruptFlag = true;
+
+    @Override
+    public void run(){
+        while (interruptFlag) {
+            System.out.println("Hello world!");
+        }
+        System.out.println("T1: Interrupted by Main");
+    }
+
+    public void setInterruptFlag() {
+        synchronized ((Object) interruptFlag) {
+            interruptFlag = false;
+        }
+    }
+}
---
+++ Task_4/src/main/java/Threads.java
@@ -0,0 +1,15 @@
+import static java.lang.Thread.sleep;
+
+/**
+ * Task 4
+ */
+public class Threads {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        ThreadPrinter firstThread = new ThreadPrinter();
+        firstThread.start();
+        sleep(2000);
+        firstThread.setInterruptFlag();
+    }
+}
