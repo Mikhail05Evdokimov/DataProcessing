f7de110a8176283101a22126b55cd90069174616
Mikhail <evdokimm05@gmail.com>
2023-09-25 11:27:50 +0700

    package fix

--- Task_1/src/main/java/ru/nsu/evdokimov/Threads.java
+++ Task_1/src/main/java/ru/nsu/evdokimov/Threads.java
@@ -1,3 +1,5 @@
+package ru.nsu.evdokimov;
+
 /**
  * Task 1
  */
@@ -6,6 +8,11 @@ public class Threads {
     public static void main(String[] args) {
         ThreadPrinter firstThread = new ThreadPrinter();
         firstThread.start();
+        System.out.println("World!");
+        System.out.println("World!");
+        System.out.println("World!");
+        System.out.println("World!");
+        System.out.println("World!");
         System.out.println("World!");
         System.out.println("World!");
         System.out.println("World!");
