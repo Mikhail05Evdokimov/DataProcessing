7f0bfafbf65c957ca75589e9ab69d6cc87fdb81c
Mikhail <evdokimm05@gmail.com>
2023-10-03 14:50:44 +0700

    one string list instead of four

--- Task_3/src/main/java/ru/nsu/evdokimov/Threads.java
+++ Task_3/src/main/java/ru/nsu/evdokimov/Threads.java
@@ -11,29 +11,30 @@ public class Threads {
     public static void main(String[] args) {
 
         List<String> firstList = new ArrayList<>();
+
         firstList.add("T1: Hello");
         firstList.add("T1: World");
         firstList.add("T1: !");
-
-        List<String> secondList = new ArrayList<>();
-        secondList.add("T2: Hello");
-        secondList.add("T2: World");
-        secondList.add("T2: !");
-
-        List<String> thirdList = new ArrayList<>();
-        thirdList.add("T3: Hello");
-        thirdList.add("T3: World");
-        thirdList.add("T3: !");
-
-        List<String> fourthList = new ArrayList<>();
-        fourthList.add("T4: Hello");
-        fourthList.add("T4: World");
-        fourthList.add("T4: !");
-
         ThreadPrinter firstThread = new ThreadPrinter(firstList);
-        ThreadPrinter secondThread = new ThreadPrinter(secondList);
-        ThreadPrinter thirdThread = new ThreadPrinter(thirdList);
-        ThreadPrinter fourthThread = new ThreadPrinter(fourthList);
+        firstList.clear();
+
+        firstList.add("T2: Hello");
+        firstList.add("T2: World");
+        firstList.add("T2: !");
+        ThreadPrinter secondThread = new ThreadPrinter(firstList);
+        firstList.clear();
+
+        firstList.add("T3: Hello");
+        firstList.add("T3: World");
+        firstList.add("T3: !");
+        ThreadPrinter thirdThread = new ThreadPrinter(firstList);
+        firstList.clear();
+
+        firstList.add("T4: Hello");
+        firstList.add("T4: World");
+        firstList.add("T4: !");
+        ThreadPrinter fourthThread = new ThreadPrinter(firstList);
+        firstList.clear();
 
         firstThread.start();
         secondThread.start();
