329473447f03d11fd8792a1283f014e9b995e50d
Mikhail <evdokimm05@gmail.com>
2023-10-21 18:24:06 +0700

    task_9 initial commit

---
+++ Task_9/src/main/java/ru/nsu/evdokimov/Fork.java
@@ -0,0 +1,13 @@
+package ru.nsu.evdokimov;
+
+public class Fork {
+    private final int id;
+
+    public Fork(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
