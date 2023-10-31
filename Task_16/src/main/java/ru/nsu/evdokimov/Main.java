3e7fcc242d4d430bd6b73a6b2ccba78672f678dd
Mikhail <evdokimm05@gmail.com>
2023-10-31 13:06:02 +0700

    modules, normal input, space waiting

--- Task_16/src/main/java/ru/nsu/evdokimov/Main.java
+++ Task_16/src/main/java/ru/nsu/evdokimov/Main.java
@@ -6,8 +6,10 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class SimpleHttpClient {
 
+public class Main {
+
+    private final static int lineLimit = 25;
     public static void main(String[] args) throws IOException {
 
         if (args.length != 1) {
@@ -20,22 +22,33 @@ public class SimpleHttpClient {
         HttpURLConnection connection = (HttpURLConnection) url.openConnection(); // открываем HTTP-соединение
 
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-            int lineCount = 0;
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-                lineCount++;
 
-                if (lineCount == 25) {
-                    System.out.println("Press space to scroll down.");
-                    System.in.read(); // ожидание нажатия пробела для прокрутки
-                    lineCount = 0;
-                }
-            }
+            printBody(reader);
+
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
             connection.disconnect(); // закрываем HTTP-соединение
         }
     }
+
+    private static void printBody(BufferedReader reader) throws IOException {
+        int lineCount = 0;
+        String line;
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+            lineCount++;
+
+            if (lineCount == lineLimit) {
+                System.out.println("Press space to scroll down.");
+                char c = (char) System.in.read(); // ожидание нажатия пробела для прокрутки
+                while (c != ' ') {
+                    c = (char) System.in.read();
+                }
+                lineCount = 0;
+            }
+        }
+    }
+
 }
+
