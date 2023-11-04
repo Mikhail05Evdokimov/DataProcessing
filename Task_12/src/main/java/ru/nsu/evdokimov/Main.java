c56fb57e616cef2e153aa4933e750517e0168e9f
Mikhail <evdokimm05@gmail.com>
2023-11-04 17:33:48 +0700

    fix *for(;;)*, LinkedList and string > 80

--- Task_12/src/main/java/ru/nsu/evdokimov/Main.java
+++ Task_12/src/main/java/ru/nsu/evdokimov/Main.java
@@ -1,27 +1,21 @@
 package ru.nsu.evdokimov;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Task 12
  */
 public class Main {
 
-    public static final List<String> stringList = new ArrayList<>();
+    public static final List<String> stringList = new LinkedList<>();
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         ChildThread childThread = new ChildThread();
         childThread.start();
-        for (;;) {
-            String string = scan.nextLine();
-            if (Objects.equals(string, "stop")) {
-                childThread.interrupt();
-                break;
-            }
+        String string;
+        do {
+            string= scan.nextLine();
             if (string.isEmpty()) {
                 synchronized (stringList) {
                     System.out.println(stringList);
@@ -29,9 +23,14 @@ public class Main {
             }
             else {
                 synchronized (stringList) {
+                    while (string.length() > 10) {
+                        stringList.add(string.substring(0, 9));
+                        string = string.substring(10);
+                    }
                     stringList.add(string);
                 }
             }
-        }
+        } while (!(Objects.equals(string, "stop")));
+        childThread.interrupt();
     }
 }
