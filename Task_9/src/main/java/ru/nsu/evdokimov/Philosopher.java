b4ad147f56b892516cba5c2f82787e456b6ae22c
Mikhail <evdokimm05@gmail.com>
2023-11-13 21:49:30 +0700

    trying to make it non-deadlock

--- Task_9/src/main/java/ru/nsu/evdokimov/Philosopher.java
+++ Task_9/src/main/java/ru/nsu/evdokimov/Philosopher.java
@@ -1,17 +1,23 @@
 package ru.nsu.evdokimov;
 
+import java.util.concurrent.Semaphore;
+
 public class Philosopher extends Thread{
 
     private int hunger;
     private final int id;
     private final Fork leftFork;
     private final Fork rightFork;
+    private final int eatTime;
+    private final int thinkTime;
 
     public Philosopher(int id, int hunger, Fork leftFork, Fork rightFork) {
         this.id = id;
         this.hunger = hunger;
         this.leftFork = leftFork;
         this.rightFork = rightFork;
+        thinkTime = 300;
+        eatTime = 300;
     }
 
     public long getId() {
@@ -27,28 +33,36 @@ public class Philosopher extends Thread{
                 throw new RuntimeException(e);
             }
             try {
-                getForks();
+                while (true) {
+                    if (getForks()) break;
+                }
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-
-            hunger--;
         }
-        System.out.println(this.getId() + " Phil go home");
+        System.out.println(this.getId() + " Phil went home");
     }
 
     private void thinking() throws InterruptedException {
-        sleep(3000);
+        sleep(thinkTime);
         System.out.println(this.getId() + " Phil finish thoughts");
     }
 
-    private void getForks() throws InterruptedException {
-        synchronized (leftFork) {
-            synchronized (rightFork) {
-                sleep(3000);
+    private boolean getForks() throws InterruptedException {
+        if (leftFork.getFork()) {
+            if (rightFork.getFork()) {
+                sleep(eatTime);
                 System.out.println(this.getId() + " Got food");
+                hunger--;
+                rightFork.unlock();
+                leftFork.unlock();
+                return true;
+            }
+            else {
+                leftFork.unlock();
             }
         }
+        Semaphore sem = new Semaphore(1, true);
+        return false;
     }
-
 }
