14f5dca8621b24f6b3fe35e40bc6a427519f5710
Mikhail <evdokimm05@gmail.com>
2023-12-04 22:08:03 +0700

    smart data partition between threads

--- Clos_Task_3/src/Main.clj
+++ Clos_Task_3/src/Main.clj
@@ -1,18 +1,16 @@
 (ns Main)
 
-(defn parallel-filter [f coll]
+(defn parallel-filter [n f coll]
   (if (empty? coll)
     coll
-    (let [;n 4                                                  ; n - Number of threads (futures)
-          ;parts (partition-all (/ (count coll) n) coll)       ; Partition the collection into n parts
-          parts (partition-all 2000001 coll)
+    (let [
+          parts (partition-all (/ n 4) coll)                ; 4 - number of threads, n/4 - numbers set size
           futures (map #(future (filter f %)) parts)]       ; Put each part in a separate future
       (apply concat (map deref futures)))))
 
 (def data (range))
-(def result (parallel-filter even? data))
 
-(time (doall (take 4000000 result)))
+(defn take-from-parallel-filter [n f coll] (take n (parallel-filter n f coll)))
+
+(time (doall (take-from-parallel-filter 4000000 even? data)))
 (time (doall (take 4000000 (filter even? data))))
-
-
